77fc8df3dc5ac3f7824b4970292c13ebae0a6502
k5f9d <bogdansavelev24@gmail.com>
2026-02-05 20:37:12 +0700

    edit Backend

--- Backend/main.go
+++ Backend/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/weather", weatherHendler)
 	http.HandleFunc("/discord", discordHandler)
-	http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
 }
