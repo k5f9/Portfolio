046d004ce43307fcfae1d6d53ae282f9f6e1d7dc
k5f9d <bogdansavelev24@gmail.com>
2026-02-05 22:16:21 +0700

    fix to get weather data(test9)

--- Frontend/app/pages/profile.vue
+++ Frontend/app/pages/profile.vue
@@ -209,8 +209,8 @@
         >
         <h1 class="absolute top-1 right-3 opacity-50 text-sm">~/weather.ts</h1>
         <h1 class="absolute top-1 left-3 md:opacity-50 opacity-0 ont-semibold text-black/70">Barnaul, Russia</h1>
-        <h1 class="absolute top-16 text-2xl text-slate-400 font-bold" >{{ weather }}</h1>
-        <h1 class="absolute font-semibold md:text-8xl text-6xl bottom-5 opacity-70 " >{{ weather }}°C</h1>
+        <h1 class="absolute top-16 text-2xl text-slate-400 font-bold" >{{ weather['temp'] }}</h1>
+        <h1 class="absolute font-semibold md:text-8xl text-6xl bottom-5 opacity-70 " >{{ weather['condition'] }}°C</h1>
         <!-- <h1 class="md:text-5xl text-2xl font-bold text-slate-500" v-else>Not found</h1> -->
         <!-- <pre>{{ weather }}</pre> -->
       </div>
